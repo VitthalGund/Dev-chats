1cd028bef62ed45583c17e8ec823bafa9f1bdb15
VitthalGund <vitthalgund30@gmail.com>
2023-11-30 19:17:33 +0530

    context api and all the routes, components are added into App.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,10 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { AuthProvider } from "./context/AuthContext.tsx";
+import { BrowserRouter } from "react-router-dom";
+import { SocketProvider } from "./context/SocketContext.tsx";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <SocketProvider>
+          <App />
+        </SocketProvider>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
