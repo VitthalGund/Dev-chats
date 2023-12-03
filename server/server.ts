40a9b9b10b9b5442aef31c96aa608f3688647bd5
VitthalGund <vitthalgund30@gmail.com>
2023-12-03 17:16:46 +0530

    All the model, controllers and  routes added type dependencies added

--- server/server.ts
+++ server/server.ts
@@ -1,16 +1,159 @@
-import express, { Express, Request, Response, Application } from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
+import session from "express-session";
+import fs from "fs";
+import { createServer } from "http";
+import passport from "passport";
+import path from "path";
+import { Server } from "socket.io";
+import { dbInstance } from "./config/db";
+import { initializeSocketIO } from "./socket/index.js";
+import { ApiError } from "./utils/ApiError.js";
+import { ApiResponse } from "./utils/ApiResponse.js";
 import dotenv from "dotenv";
+import connectDB from "./config/db";
 
-//For env File
-dotenv.config();
-
-const app: Application = express();
-const port = process.env.PORT || 8000;
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Welcome to Express & TypeScript Server");
+dotenv.config({
+  path: "./.env",
 });
 
-app.listen(port, () => {
-  console.log(`Server is Fire at http://localhost:${port}`);
+/**
+ * Starting from Node.js v14 top-level await is available and it is only available in ES modules.
+ * This means you can not use it with common js modules or Node version < 14.
+ */
+const majorNodeVersion = +process.env.NODE_VERSION!?.split(".")[0] || 0;
+
+const app = express();
+
+const httpServer = createServer(app);
+
+const startServer = () => {
+  httpServer.listen(process.env.PORT || 8080, () => {
+    console.info(
+      `📑 Visit the documentation at: http://localhost:${
+        process.env.PORT || 8080
+      }`
+    );
+    console.log("⚙️  Server is running on port: " + process.env.PORT);
+  });
+};
+
+if (majorNodeVersion >= 14) {
+  try {
+    connectDB().then(() => startServer());
+    // startServer();
+  } catch (err) {
+    console.log("Mongo db connect error: ", err);
+  }
+} else {
+  connectDB()
+    .then(() => {
+      startServer();
+    })
+    .catch((err) => {
+      console.log("Mongo db connect error: ", err);
+    });
+}
+
+// const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname("server.ts");
+
+const io = new Server(httpServer, {
+  pingTimeout: 60000,
+  cors: {
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  },
 });
+
+app.set("io", io); // using set method to mount the `io` instance on the app to avoid usage of `global`
+
+// global middlewares
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public")); // configure static file to save images locally
+app.use(cookieParser());
+
+// required for passport
+app.use(
+  session({
+    secret: process.env.EXPRESS_SESSION_SECRET!,
+    resave: true,
+    saveUninitialized: true,
+  })
+); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
+// api routes
+import { errorHandler } from "./middlewares/error.middlewares.js";
+
+// * App routes
+import userRouter from "./routes/auth.js";
+
+import chatRouter from "./routes/chat.js";
+import messageRouter from "./routes/message.js";
+
+// * Seeding handlers
+import { seedChatApp } from "./seeds/chat-app.seeds.js";
+import { getGeneratedCredentials, seedUsers } from "./seeds/user.js";
+
+// * App apis
+app.use("/api/v1/users", userRouter);
+
+app.use("/api/v1/chat-app/chats", chatRouter);
+app.use("/api/v1/chat-app/messages", messageRouter);
+
+// * Seeding
+app.post("/api/v1/seed/chat-app", seedUsers, seedChatApp);
+
+initializeSocketIO(io);
+
+// ! 🚫 Danger Zone
+app.delete("/api/v1/reset-db", async (req, res) => {
+  if (dbInstance) {
+    // Drop the whole DB
+    await dbInstance.connection.db.dropDatabase({
+      dbName: "DevChats",
+    });
+
+    const directory = "./public/images";
+
+    // Remove all product images from the file system
+    fs.readdir(directory, (err, files) => {
+      if (err) {
+        // fail silently
+        console.log("Error while removing the images: ", err);
+      } else {
+        for (const file of files) {
+          if (file === ".gitkeep") continue;
+          fs.unlink(path.join(directory, file), (err) => {
+            if (err) throw err;
+          });
+        }
+      }
+    });
+    // remove the seeded users if exist
+    fs.unlink("./public/temp/seed-credentials.json", (err) => {
+      // fail silently
+      if (err) console.log("Seed credentials are missing.");
+    });
+    return res
+      .status(200)
+      .json(new ApiResponse(200, null, "Database dropped successfully"));
+  }
+  throw new ApiError(500, "Something went wrong while dropping the database");
+});
+
+// common error handling middleware
+app.use(errorHandler);
+
+export { httpServer };
